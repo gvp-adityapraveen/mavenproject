23d41ff8c842f7177f93ef8592d9ffe644d937f8
aditya.gollakota <aditya.gollakota@kony.com>
2014-08-21 19:17:29 +0530

    To check build trigger-4

--- src/test/java/com/companyname/bank/AppTest.java
+++ src/test/java/com/companyname/bank/AppTest.java
@@ -34,6 +34,6 @@ public class AppTest
      */
     public void testApp()
     {
-        assertEquals( "aditya prave","aditya prave");
+        assertEquals( "aditya prav","aditya prav");
     }
 }
