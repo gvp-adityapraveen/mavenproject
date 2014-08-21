f4c18a62674f0136ef7961e08ccc606d111c57ad
aditya.gollakota <aditya.gollakota@kony.com>
2014-08-21 18:19:14 +0530

    commented

--- src/test/java/com/companyname/bank/AppTest.java
+++ src/test/java/com/companyname/bank/AppTest.java
@@ -10,7 +10,7 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-	String salutation = System.getProperty("salute");
+//	String salutation = System.getProperty("salute");
     /**
      * Create the test case
      *
